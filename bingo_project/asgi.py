0b561aa9c18167f4549082608406f90cab6c2849
screwdatt <146767746+screwdatt@users.noreply.github.com>
2025-12-06 11:33:16 -0500

    Update asgi.py

--- bingo_project/asgi.py
+++ bingo_project/asgi.py
@@ -1,1 +1,13 @@
 
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import cards.routing
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bingo_project.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(URLRouter(cards.routing.websocket_urlpatterns)),
+})
